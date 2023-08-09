d8e1562aff305ac1b73652f3771944408ee0712f
Solomon Odunusi <solomonodunsui@gmail.com>
2023-08-08 19:17:21 -0700

    Fixed task 1, 3 and 5 errors

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print(f"{number}")
-    else:
-        print(f"{number:02}", end=", ")
+for num in range(99):
+    print(f"{num:02d}", end=", ")
+print(f"{99:02d}")

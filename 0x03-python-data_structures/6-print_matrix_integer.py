e54a0c0ac390d0ae0146f8ad2453b1e5e59d5469
Solomon Odunusi <solomonodunsui@gmail.com>
2023-08-13 21:37:42 -0700

    Added an if else statement to print a space at the end

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for column in row:
             print("{:d}".format(column), end=" " if column != row[-1] else "")
+            if column != row[-1]:
+                print(end=" ")
+            else:
+                print("")
         print()

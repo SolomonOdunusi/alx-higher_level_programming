9e8361825c0a45d8892c61a7cb5611ae5753ff8f
Solomon Odunusi <solomonodunsui@gmail.com>
2023-08-30 17:57:24 -0700

    Changed pass to break

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -19,7 +19,7 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end=" ")
             j += 1
     except IndexError:
-        pass
+        break
     finally:
-        print("")
+        print()
     return (j)

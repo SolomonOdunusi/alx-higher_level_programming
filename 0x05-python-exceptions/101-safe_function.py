374d348dd653ff5df76ba1e6d1fbb45058843bca
Solomon Odunusi <solomonodunsui@gmail.com>
2023-08-30 17:51:10 -0700

    Debugged safe_function func

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,9 +4,10 @@ A func that executes a func safely
 """
 import sys
 
+
 def safe_function(fct, *args):
     try:
         return (fct(*args))
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
         return (None)

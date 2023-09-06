404c7876666005c39e771f23c1ad1ae23ba7d957
Solomon Odunusi <solomonodunsui@gmail.com>
2023-09-05 19:36:00 -0700

    Debugged code

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""Magic string"""
-
-
 def magic_string():
-    """Prints BestSchool multiple times."""
     magic_string.count = getattr(magic_string, 'count', 0) + 1
     return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")

99b97287a53be9d2b0c72a5dfd0950df86b38619
Solomon Odunusi <solomonodunsui@gmail.com>
2023-09-05 19:38:25 -0700

    Debugged code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,8 +4,4 @@
 
 class LockedClass:
     """Prevent the user from making new inst attr"""
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            self.__dict__[name] = value
-        else:
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
+    __slots__ = ["first_name"]

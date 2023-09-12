e0bd9daedad926e9c131c11f0e74bb13c872dffe
Solomon Odunusi <solomonodunsui@gmail.com>
2023-09-12 16:27:26 -0700

    Fixed 0 task

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+A module that reads a text file (UTF8)
+and prints it to stdout
+"""
+
+
 def read_file(filename=""):
     """A func that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:

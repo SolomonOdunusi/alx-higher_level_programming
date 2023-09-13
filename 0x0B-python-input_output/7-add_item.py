223197121f58944c4ea8d3398fb1ccd11d9e5e83
Solomon Odunusi <solomonodunsui@gmail.com>
2023-09-12 17:22:42 -0700

    Debugging task 8

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Write a script that adds all arg to a Py list, and save them to a file"""
+"""
+Write a script that adds all arguments to a Python list
+and then save them to a file
+"""
 
 
 import sys
 import os.path
-
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-if os.path.exists('add_item.json'):
-    my_list = load_from_json_file('add_item.json')
-else:
-    my_list = []
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-save_to_json_file(my_list, 'add_item.json')
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+my_list = []
+if os.path.isfile("add_item.json"):
+    my_list = load_from_json_file("add_item.json")
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+save_to_json_file(my_list, "add_item.json")

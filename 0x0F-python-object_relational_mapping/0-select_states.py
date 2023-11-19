713f6c35ca743a3e5c6790dfb941b14ecfc7d8f0
Solomon Odunusi <solomonodunsui@gmail.com>
2023-11-19 06:19:10 -0800

    Added script to list states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
+""" Write a script that lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Get MySQL credentials from command line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # Execute the SQL query to select all states and order by states.id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-
-    # Display the results
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    """In order to put our new connnection to good use we
+     need to create a cursor object"""
+    cur = db.cursor()
+    """The execute function requires one parameter, the query."""
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    """Obtaining Query Results"""
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    # Close the cursor and connection
-    cursor.close()
+    """ Close all cursors"""
+    cur.close()
+    """Close all databases"""
     db.close()

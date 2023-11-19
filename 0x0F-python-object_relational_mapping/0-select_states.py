c15492e7c27605e2505df70fe4d6573939b7d77b
Solomon Odunusi <solomonodunsui@gmail.com>
2023-11-19 06:23:29 -0800

    Added script to list states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,21 +4,18 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+
+    username, password, database = sys.argv[1], sysargv[2], sysargv[3]
+
     db = MySQLdb.connect(host='localhost',
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3],
                          port=3306)
-    """In order to put our new connnection to good use we
-     need to create a cursor object"""
     cur = db.cursor()
-    """The execute function requires one parameter, the query."""
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    """Obtaining Query Results"""
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    """ Close all cursors"""
     cur.close()
-    """Close all databases"""
     db.close()

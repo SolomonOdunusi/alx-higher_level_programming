d22b467e5ae9c0b1465eb20800c7e2847240cb20
Solomon Odunusi <solomonodunsui@gmail.com>
2023-11-19 07:08:24 -0800

    Added script to filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
+"""A scirpt that lists states"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-
+    """ It filters the states"""
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -18,16 +13,12 @@ if __name__ == "__main__":
         passwd=password,
         db=database
     )
-
-
     cursor = db.cursor()
-
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
+    cursor.execute(
+        "SELECT * FROM statesWHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cursor.fetchall()
 
     for row in rows:
         print(row)
     cursor.close()
     db.close()
-

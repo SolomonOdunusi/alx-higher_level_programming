f3937f3e350cfd5601d719a8ccb69db1bd067321
Solomon Odunusi <solomonodunsui@gmail.com>
2023-11-30 21:04:49 -0800

    Change comment

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/usr/bash
-# A script that takes a URL and
-# displays size of the body of the resp
+# A script that takes a URL and displays size of the body of the resp
 curl -sI "$1" | grep Content-Length | awk '{print $2}'

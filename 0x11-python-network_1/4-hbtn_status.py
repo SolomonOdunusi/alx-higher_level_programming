55d22dfd76e2c132611d62943d91a3a09079acde
Solomon Odunusi <solomonodunsui@gmail.com>
2023-12-03 21:31:09 -0800

    Debugging 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,8 +5,7 @@ import requests
 if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
 
-    resp = requests.get(url)
-    status = resp.text
+    status = requests.get(url)
 
     print("Body response:")
     print("\t- type: {}".format(type(status)))

a43183c9585d57f2fa249127befcaa33c96debf5
Solomon Odunusi <solomonodunsui@gmail.com>
2023-12-03 21:49:08 -0800

    Fixed the pycodestyle issues

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Send a POST request to the passed URL 
+"""Send a POST request to the passed URL
 with the email as a parameter,
 and finally displays the body of the response"""
 import requests

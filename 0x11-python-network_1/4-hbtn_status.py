83518cbf52a711d0306abca0b3cf98001e33b965
Solomon Odunusi <solomonodunsui@gmail.com>
2023-12-03 21:32:39 -0800

    Debugging 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""  
+"""
+Fetches https://alx-intranet.hbtn.io/status using the requests package
+"""
+
 import requests
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-
-    status = requests.get(url)
-
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    content = response.text
     print("Body response:")
-    print("\t- type: {}".format(type(status)))
-    print("\t- content: {}".format(status))
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+

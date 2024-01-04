06ef290dc1c32eb56be92dd45049359be71c837d
SolomonOdunusi <solomonodunusi@gmail.com>
2024-01-03 17:30:31 -0800

    Debugged starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,6 @@ const url = process.argv[2];
 
 let count = 0;
 request(url, function (err, resp, body) {
-
   if (resp.statusCode === 200) {
     for (const result of JSON.parse(body).results) {
       for (const character of result.characters) {
